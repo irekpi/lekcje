58dba9523f0e7a7625f1325d302a0d289163a12b
irekpi <pilat.irek@gmail.com>
2020-03-10 20:08:56 +0100

    Renamed files

--- openx/wariant2.py
+++ openx/wariant2.py
@@ -1,48 +1,104 @@
-import urllib.request
-import json
-# urllib.request.urlopen("https://jsonplaceholder.typicode.com/posts") as post_url:
-# json_post = urllib.urlopen(url_post)
-
-# POST GETTER
-url_post = "https://jsonplaceholder.typicode.com/posts"
-json_post = urllib.request.urlopen(url_post)
-post_data = json.loads(json_post.read())
-
-# USER GETTER
-url_user = "https://jsonplaceholder.typicode.com/users"
-json_user = urllib.request.urlopen(url_user)
-user_data = json.loads(json_user.read())
-
-baza = {}
-
-for i in user_data:
-    user = i['id']
-    #print(user)
-    for j in post_data:
-        post = j['userId']
-        #print (post)
-        if post == user:
-            user_post = 'user_post'
-            baza.setdefault(user_post, []).append([
-                j['userId'],
-                j['id'],
-                j['title'],
-                j['body']
-            ])
+import requests
+from pprint import pprint
+from math import dist
 
 
-print(type(user_data))
-print(type(baza))
+def _get(url):
+    try:
+        response = requests.get(url)
+    except requests.RequestException:
+        raise Exception('Cannot ')
+    else:
+        if response.status_code != 200:
+            print('Cannot download data')
+            return None
+    return response
 
 
-# posts = {
-#     'apple': 2,
-#     'banana': 'called',
-# }
-# if post_data['user_id'] == user_data['id']:
-#     posts.append([{
-#         post_data['id'],
-#         post_data['title'],
-#         post_data['body']}])
-# print(post_data)
-# print(user_data['name'])
+post_data = _get('https://jsonplaceholder.typicode.com/posts').json()
+user_data = _get('https://jsonplaceholder.typicode.com/users').json()
+
+
+def script():
+    base = {}
+    for user in user_data:
+        user_id = user['id']
+        for post in post_data:
+            user_post = post['userId']
+            if user_post == user_id:
+                base.setdefault(user_id, {'user_name': user.get('name', 'No name')}).setdefault('posts', []).append({
+                    'id': post['id'],
+                    'title': post['title'],
+                    'body': post['body'],
+                })
+
+    pprint(base)
+
+
+def counter():
+    count = {}
+
+    for item in post_data:
+        if item['userId'] in count:
+            count[item['userId']] += 1
+        else:
+            count[item['userId']] = 1
+
+    for i in user_data:
+        print('User {} posted: {} posts.'.format(i['username'], max(*count)))
+
+
+def unique():
+    unique_title = []
+
+    for item in post_data:
+        title = item['title']
+        if title not in unique_title:
+            unique_title.append(title)
+    pprint(unique_title)
+
+
+def get_user_data(user):
+    try:
+        geo = user['address']['geo']
+        lat = float(geo['lat'])
+        lng = float(geo['lng'])
+        return lat, lng, user['name']
+    except KeyError as error:
+        print(error)
+        raise
+
+
+def distance():
+    user_data = _get('https://jsonplaceholder.typicode.com/users').json()
+    result = []
+
+    for user1 in user_data:
+        shortest_dist = None
+        from_lat, from_lng, from_user = get_user_data(user1)
+        user2_name = ''
+        print('___')
+        # del user_data[0]
+        for user2 in user_data:
+            to_lat, to_lng, to_user = get_user_data(user2)
+            if from_user == to_user:
+                continue
+            way = dist([from_lat, from_lng], [to_lat, to_lng])
+            if shortest_dist is None or way < shortest_dist:
+                user2_name = to_user
+                shortest_dist = way
+            print(way, shortest_dist)
+        result.append({
+            'from_user': from_user,
+            'to_user': user2_name,
+            'shortest_dist': shortest_dist
+        })
+    return result
+
+
+if __name__ == '__main__':
+    # script()
+    # counter()
+    # unique()
+    distance()
+
--- openx/wariant22.py
+++ openx/wariant22.py
@@ -1,22 +1,17 @@
-import requests
+import urllib.request
+import json
 from pprint import pprint
-from math import dist
 
 
-def _get(url):
-    try:
-        response = requests.get(url)
-    except requests.RequestException:
-        raise Exception('Cannot ')
-    else:
-        if response.status_code != 200:
-            print('Cannot download data')
-            return None
-    return response
+# POST GETTER
+url_post = "https://jsonplaceholder.typicode.com/posts"
+json_post = urllib.request.urlopen(url_post)
+post_data = json.loads(json_post.read())
 
-
-post_data = _get('https://jsonplaceholder.typicode.com/posts').json()
-user_data = _get('https://jsonplaceholder.typicode.com/users').json()
+# USER GETTER
+url_user = "https://jsonplaceholder.typicode.com/users"
+json_user = urllib.request.urlopen(url_user)
+user_data = json.loads(json_user.read())
 
 
 def script():
@@ -35,69 +30,5 @@ def script():
     pprint(base)
 
 
-def counter():
-    count = {}
-
-    for item in post_data:
-        if item['userId'] in count:
-            count[item['userId']] += 1
-        else:
-            count[item['userId']] = 1
-
-    for i in user_data:
-        print('User {} posted: {} posts.'.format(i['username'], max(*count)))
-
-
-def unique():
-    unique_title = []
-
-    for item in post_data:
-        title = item['title']
-        if title not in unique_title:
-            unique_title.append(title)
-    pprint(unique_title)
-
-
-def get_user_data(user):
-    try:
-        geo = user['address']['geo']
-        lat = float(geo['lat'])
-        lng = float(geo['lng'])
-        return lat, lng, user['name']
-    except KeyError as error:
-        print(error)
-        raise
-
-
-def distance():
-    user_data = _get('https://jsonplaceholder.typicode.com/users').json()
-    result = []
-
-    for user1 in user_data:
-        shortest_dist = None
-        from_lat, from_lng, from_user = get_user_data(user1)
-        user2_name = ''
-        print('___')
-        # del user_data[0]
-        for user2 in user_data:
-            to_lat, to_lng, to_user = get_user_data(user2)
-            if from_user == to_user:
-                continue
-            way = dist([from_lat, from_lng], [to_lat, to_lng])
-            if shortest_dist is None or way < shortest_dist:
-                user2_name = to_user
-                shortest_dist = way
-            print(way, shortest_dist)
-        result.append({
-            'from_user': from_user,
-            'to_user': user2_name,
-            'shortest_dist': shortest_dist
-        })
-    return result
-
-
 if __name__ == '__main__':
-    # script()
-    # counter()
-    # unique()
-    distance()
+    script()
